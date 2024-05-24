c49181c58f4d63dd7eef6b69cbc029d1b488dc31
islanderian <createdby.island@gmail.com>
2024-05-25 01:06:10 +0900

    #2.0 Painting Lines

--- app.js
+++ app.js
@@ -4,17 +4,26 @@ const ctx = canvas.getContext("2d");
 // canvas 사이즈 명시하기
 canvas.width = 800;
 canvas.height = 800;
+ctx.lineWidth = 2;
 
-// 몸통
-ctx.fillRect(210 - 40, 200 - 20, 15, 100);
-ctx.fillRect(350 - 40, 200 - 20, 15, 100);
-ctx.fillRect(260 - 40, 200 - 20, 60, 200);
+const colors = [
+  "#ff3838",
+  "#ffb8b8",
+  "#c56cf0",
+  "#ff9f1a",
+  "#fff200",
+  "#32ff7e",
+  "#7efff5",
+];
 
-ctx.arc(250, 100, 50, 0, 2 * Math.PI); // 원형 그리기 (얼굴)
-ctx.fill();
+function onClickCanvas(e) {
+  const color = colors[Math.floor(Math.random() * colors.length)];
+  ctx.strokeStyle = color;
 
-ctx.beginPath(); // 새로운 Path
-ctx.fillStyle = "red"; // 색상 바꾸기
-ctx.arc(260 + 10, 80, 8, 0, 2 * Math.PI); // 붉은 원형 그리기 (눈)
-ctx.arc(220 + 10, 80, 8, Math.PI, 2 * Math.PI);
-ctx.fill();
+  ctx.beginPath();
+  ctx.moveTo(0, 0);
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
+}
+
+canvas.addEventListener("mousemove", onClickCanvas);
