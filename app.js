0a77cc9472dc94a283dc65b07594d30378fa4413
islanderian <createdby.island@gmail.com>
2024-05-24 20:50:11 +0900

    #1.2 Paths

--- app.js
+++ app.js
@@ -5,5 +5,14 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-// 채워진 직사각형 그리기
-ctx.fillRect(50, 50, 100, 200);
+// 채워진 직사각형 그리기 (rect()와 fill()을 단축한 함수)
+// ctx.fillRect(50, 50, 100, 200);
+ctx.rect(50, 50, 100, 100);
+ctx.rect(150, 150, 100, 100);
+ctx.rect(250, 250, 100, 100);
+ctx.fill();
+
+ctx.beginPath(); // 새로운 경로 만들기
+ctx.rect(350, 350, 100, 100);
+ctx.fillStyle = "red";
+ctx.fill();
