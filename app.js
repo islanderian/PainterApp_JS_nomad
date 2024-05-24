024e083d6b18a8b1e5866f34ff5aeb7d0405a506
islanderian <createdby.island@gmail.com>
2024-05-24 20:58:58 +0900

    #1.3 moveTo and lineTo

--- app.js
+++ app.js
@@ -5,14 +5,10 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-// 채워진 직사각형 그리기 (rect()와 fill()을 단축한 함수)
-// ctx.fillRect(50, 50, 100, 200);
-ctx.rect(50, 50, 100, 100);
-ctx.rect(150, 150, 100, 100);
-ctx.rect(250, 250, 100, 100);
-ctx.fill();
-
-ctx.beginPath(); // 새로운 경로 만들기
-ctx.rect(350, 350, 100, 100);
-ctx.fillStyle = "red";
-ctx.fill();
+ctx.moveTo(50, 50); // 해당 좌표로 이동 (그리지 않으면서)
+ctx.lineTo(150, 50); // 해당 좌표로 이동 (그리면서)
+ctx.lineTo(150, 150);
+ctx.lineTo(50, 150);
+ctx.lineTo(50, 50);
+ctx.stroke(); // 선으로 그리기
+ctx.fill(); // 채우기
