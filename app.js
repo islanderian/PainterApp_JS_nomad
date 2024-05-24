205e6691f5645faec8ba825106da596dbd4d0835
islanderian <createdby.island@gmail.com>
2024-05-24 21:12:08 +0900

    #1.4 Drawing Project One

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.moveTo(50, 50); // 해당 좌표로 이동 (그리지 않으면서)
-ctx.lineTo(150, 50); // 해당 좌표로 이동 (그리면서)
-ctx.lineTo(150, 150);
-ctx.lineTo(50, 150);
-ctx.lineTo(50, 50);
-ctx.stroke(); // 선으로 그리기
-ctx.fill(); // 채우기
+ctx.fillRect(200, 200, 50, 200);
+ctx.fillRect(400, 200, 50, 200);
+// ctx.lineWidth = 2;
+ctx.fillRect(300, 300, 50, 100);
+ctx.fillRect(200, 200, 200, 20);
+ctx.moveTo(200, 200);
+ctx.lineTo(325, 100);
+ctx.lineTo(450, 200);
+// ctx.stroke();
+ctx.fill();
