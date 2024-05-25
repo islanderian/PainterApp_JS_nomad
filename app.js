0b47deb510817be9df97f4e4e21be6093614dd2e
islanderian <createdby.island@gmail.com>
2024-05-25 12:53:59 +0900

    #2.2 Line Width

--- app.js
+++ app.js
@@ -2,17 +2,21 @@ const canvas = document.querySelector("canvas");
 // canvas에 그릴 브러쉬
 const ctx = canvas.getContext("2d");
 // canvas 사이즈 명시하기
+const lineWidth = document.querySelector("#line-width");
+
 canvas.width = 800;
 canvas.height = 800;
-ctx.lineWidth = 2;
+ctx.lineWidth = lineWidth.value; // 선 굵기 초기값
 
 let isPainting = false; // 그려지는 중인지 체크
+
 function onMove(e) {
   if (isPainting) {
     ctx.lineTo(e.offsetX, e.offsetY);
     ctx.stroke();
     // return;
   }
+
   ctx.moveTo(e.offsetX, e.offsetY); // 이 라인을 if 위에 먼저 쓰면 그려지지 않음!!
 }
 function startPainting(e) {
@@ -20,9 +24,16 @@ function startPainting(e) {
 }
 function canclePainting(e) {
   isPainting = false;
+  ctx.beginPath(); // 새로운 path 생성
+}
+function onLineChange(e) {
+  ctx.lineWidth = e.target.value;
 }
 
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", canclePainting);
-canvas.addEventListener("mouseleave", canclePainting); // 마우스가 canvas 를 떠나도 그리기 종료
+// 마우스가 canvas 를 떠나도 그리기 종료
+canvas.addEventListener("mouseleave", canclePainting);
+
+lineWidth.addEventListener("change", onLineChange);
