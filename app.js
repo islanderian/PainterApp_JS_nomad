4592c28f8d8e1453d46196b1aaef92f1734814b8
islanderian <createdby.island@gmail.com>
2024-05-25 14:05:46 +0900

    #2.4 Paint Color part Two

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const ctx = canvas.getContext("2d");
 // canvas 사이즈 명시하기
 const lineWidth = document.querySelector("#line-width");
 const color = document.querySelector("#color");
+// HTML 요소를 Array 로 만들어 가져오기
+const colorOptions = Array.from(document.querySelectorAll(".color-option"));
 
 canvas.width = 800;
 canvas.height = 800;
@@ -17,8 +19,8 @@ function onMove(e) {
     ctx.stroke();
     // return;
   }
-
-  ctx.moveTo(e.offsetX, e.offsetY); // 이 라인을 if 위에 먼저 쓰면 그려지지 않음!!
+  // 이 라인을 if 위에 먼저 쓰면 그려지지 않음!!
+  ctx.moveTo(e.offsetX, e.offsetY);
 }
 function startPainting(e) {
   isPainting = true;
@@ -31,9 +33,17 @@ function onLineChange(e) {
   ctx.lineWidth = e.target.value;
 }
 function onColorChange(e) {
-  console.log(e.target.value);
   ctx.strokeStyle = e.target.value;
-  // ctx.fillStyle = e.target.value;
+  ctx.fillStyle = e.target.value;
+}
+function onColorClick(e) {
+  const colorValue = e.target.dataset.color;
+
+  // console.log(e.target.dataset.color);
+  ctx.strokeStyle = colorValue;
+  ctx.fillStyle = colorValue;
+  // 클릭한 색상을 color 인풋창에 표시하기
+  color.value = colorValue;
 }
 
 canvas.addEventListener("mousemove", onMove);
@@ -44,3 +54,4 @@ canvas.addEventListener("mouseleave", canclePainting);
 
 lineWidth.addEventListener("change", onLineChange);
 color.addEventListener("change", onColorChange);
+colorOptions.forEach((item) => item.addEventListener("click", onColorClick));
